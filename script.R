cf6ca6955d386d9aa3ad4ebe0a5f5d1b33670006
Adilet Malikov <malikov_a@auca.kg>
2023-12-05 01:56:40 +0600

    Add csv

--- script.R
+++ script.R
@@ -1,24 +1,8 @@
-if (!require(xlsx)) {
-  install.packages("xlsx")
-}
-library(xlsx)
-
-pathes_to_grades <- lapply(6:11, function(grade) {
-  path_template <- "data-2020/%d-grade.xlsx"
-  path <- sprintf(path_template, grade)
-  return(path)
-})
-
-data <- lapply(pathes_to_grades, function(path) {
-  read.xlsx(path, 1, colNames = TRUE)
-})
-
-grade6 <- c()
-sum <- 0.0
-
-for (n in data[[1]][[17]]) {
-  grade6 <- append(n, grade6)
-  sum <- sum + n
+if(!require('readODS')) {
+  install.packages('readODS')
+  library('readODS')
 }
 
-grade6
+names <- read_ods("data-2020/names.ods", sheet=2)
+
+write.csv(names, "data-2020/names.csv", row.names=FALSE)
